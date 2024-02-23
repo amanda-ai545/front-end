6acaf19591ca8bdff39f87d0554be6179e92dc0a
Amanda Evallo <amandadianeevallo@gmail.com>
2024-02-23 23:20:03 +0800

    feat: add Jost fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jost: ['Jost', 'san-serif'],
+      },
+    },
   },
   plugins: [],
 };

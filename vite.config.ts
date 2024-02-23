93d93c72a48c46913cfdd88fe35a27922342fd73
Amanda Evallo <amandadianeevallo@gmail.com>
2024-02-23 23:20:31 +0800

    fix: path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
@@ -13,5 +14,12 @@ export default defineConfig({
   preview: {
     port: 4300,
     host: 'localhost',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+      '@assets': `${path.resolve(__dirname, './src/assets/')}`,
+      '@components': `${path.resolve(__dirname, './src/components/')}`,
+    },
   },
 });

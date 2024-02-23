ba6513abf8cff3c503b3e675395fb251f209162e
Amanda Evallo <amandadianeevallo@gmail.com>
2024-02-23 22:26:58 +0800

    initial set up

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: __dirname,
   plugins: [react()],
-})
+  cacheDir: './node_modules/.vite/.',
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+});

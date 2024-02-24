c03778b346fe3cd7f24d5a101d1c0988660e2198
Amanda Evallo <amandadianeevallo@gmail.com>
2024-02-24 18:04:39 +0800

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import '@assets/styles/main.css';
+import InnerWrapper from '@components/common/InnerWrapper';
+import Wrapper from '@components/common/Wrapper';
+import UserList from '@components/UserList/UserList';
 
 function App() {
-  return <h1 className="text-3xl font-jost">Hello world!</h1>;
+  return (
+    <Wrapper>
+      <InnerWrapper>
+        <UserList />
+      </InnerWrapper>
+    </Wrapper>
+  );
 }
 
 export default App;
